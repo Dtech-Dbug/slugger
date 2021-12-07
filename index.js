40b334dd20929b6af4fed17b710571f8f26d2122
Dwaipayan Chakroborty <heydwaipayanhere@gmail.com>
2021-12-07 11:33:15 +0530

    added comments

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 // Only change code below this line
 function slugger(title) {
+  // split the string
   let split = title.split(" ");
   let lowerCasedSplit = [];
 
+  // lop through them and turn them to lowercase
   for (let i = 0; i < split.length; i++) {
     lowerCasedSplit.push(split[i].toLowerCase());
   }
+
+  // join them with '-'
   return lowerCasedSplit.join("-");
 }
 // Only change code above this line
